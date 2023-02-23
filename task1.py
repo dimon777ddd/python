009c0d2653f066d44dcbe216f0ee6a22f066a628
dimon777ddd <gxgamer727@gmail.com>
2023-02-23 22:16:08 +0400

    first lesson

---
+++ task1.py
@@ -0,0 +1,5 @@
+n = 700
+m = 750
+
+days = (m + n - 1) // n
+print(days)
---
+++ task3.py
@@ -0,0 +1,8 @@
+i = int(input())
+j = int(input())
+
+if i == j:
+    print("bez dopolnitelnoi informacii ne vipolnit")
+else:
+    ans = i + j - 1
+    print(ans)
